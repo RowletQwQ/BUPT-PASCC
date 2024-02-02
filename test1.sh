b2938931c80f899dc63ccc70bb13275810056ee0
张磊 <1050069707@qq.com>
2024-02-02 16:52:07 +0800

    Edit by browser

--- test1.sh
+++ test1.sh
@@ -1,4 +1,6 @@
-/usr/share/Pascal2c/pascal2c demo.pas demo.c
+/usr/share/Pascal2c/pascal2c /data/workspace/myshixun/demo.pas /data/workspace/myshixun/demo.c
+
+cd /data/workspace/myshixun/
 
 gcc demo.c -o demo
 
