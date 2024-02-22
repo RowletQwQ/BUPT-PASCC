c6bae7dd33ac3f41ec9ca504b377a2300768b954
左湘宁 <2947729419@qq.com>
2024-02-22 10:23:56 +0800

    Edit by browser

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-/usr/share/Pascal2C/build/pascal2c /data/workspace/myshixun/hidden_set/00_comment2.pas /data/workspace/myshixun/hidden_set/00_comment2.c
 cd /data/workspace/myshixun/hidden_set
+/usr/share/Pascal2C/build/pascal2c 00_main.pas 00_main.c
+
 read filename
 
 gcc $filename
