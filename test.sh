9ceb56601a7dba98e62e292db85497b2699056be
左湘宁 <2947729419@qq.com>
2024-02-22 10:06:28 +0800

    Edit by browser

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+/usr/share/Pascal2C/build/pascal2c /data/workspace/myshixun/open_set/00_mian.pas /data/workspace/myshixun/open_set/00_mian.c
 cd /data/workspace/myshixun/open_set
-/usr/share/Pascal2C/build/pascal2c 00_mian.pas 00_mian.c
 echo "Please enter filename: "
 read filename
 
