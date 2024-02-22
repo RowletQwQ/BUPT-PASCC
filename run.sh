024e3dba9491512dcb99bff9ab37330b67e5603d
左湘宁 <2947729419@qq.com>
2024-02-22 14:44:39 +0800

    Edit by browser

--- run.sh
+++ run.sh
@@ -3,3 +3,8 @@ cd /data/workspace/myshixun/open_set
 for file in *.pas; do
     /usr/share/Pascal2C/build/pascal2c  "$file" "${file%.pas}.c"
 done
+
+cd /data/workspace/myshixun/hidden_set
+for file in *.pas; do
+    /usr/share/Pascal2C/build/pascal2c  "$file" "${file%.pas}.c"
+done
