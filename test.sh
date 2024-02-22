89434dd00ba76f87cd65893a0f093e2710c7d947
左湘宁 <2947729419@qq.com>
2024-02-22 11:11:41 +0800

    Edit by browser

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /data/workspace/myshixun/open_set
 for file in *.pas; do
-    /usr/share/Pascal2C/build/pascal2c *.pas *.c
+    /usr/share/Pascal2C/build/pascal2c  "$file" "${file%.pas}.c"
 done
 #/usr/share/Pascal2C/build/pascal2c 00_main.pas 00_main.c
 
