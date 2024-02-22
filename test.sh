4a907ec09aadab0b84796563d7c5bd198560b11b
左湘宁 <2947729419@qq.com>
2024-02-22 14:51:25 +0800

    Edit by browser

--- test.sh
+++ test.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 cd /data/workspace/myshixun/open_set
-rm a.out
 #/usr/share/Pascal2C/build/pascal2c 00_main.pas 00_main.c
 
 read filename
 
 if [ ! -e "$filename" ]; then
     cd /data/workspace/myshixun/hidden_set
-    rm a.out
     if [ ! -e "$filename" ]; then
         echo "File does not exist."
         exit
