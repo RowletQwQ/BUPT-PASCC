17568e7d97f7fc0904fff7f08376c0afbf3de98f
TY Nong <60682022+RowletQwQ@users.noreply.github.com>
2024-02-29 17:23:49 +0800

    Ast interface (#2)
    * System automatically submitted
    
    * 完成ast的定义
    
    ---------
    
    Co-authored-by: 农腾跃 <pfo6kwamv@educoder.net>

---
+++ src/ast/stmt.cpp
@@ -0,0 +1,112 @@
+#include "stmt.hpp"
+#include "visitor.hpp"
+
+void ExprStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void RelExprStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void AddExprStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void MulExprStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void UnaryExprStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void PrimaryExprStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void ValueStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void NumberStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void StrStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void LValStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void FuncCallStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void PeriodStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void ConstDeclStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void VarDeclStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void FuncHeadDeclStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void FuncBodyDeclStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void FuncDeclStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void AssignStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void IfStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void ForStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void ReadFuncStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
+
+void WriteFuncStmt::accept(StmtVisitor &visitor)
+{
+    visitor.visit(*this);
+}
---
+++ src/ast/visitor.hpp
@@ -0,0 +1,49 @@
+
+class ExprStmt;
+class RelExprStmt;
+class AddExprStmt;
+class MulExprStmt;
+class UnaryExprStmt;
+class PrimaryExprStmt;
+class ValueStmt;
+class NumberStmt;
+class StrStmt;
+class LValStmt;
+class FuncCallStmt;
+class PeriodStmt;
+class ConstDeclStmt;
+class VarDeclStmt;
+class FuncHeadDeclStmt;
+class FuncBodyDeclStmt;
+class FuncDeclStmt;
+class AssignStmt;
+class IfStmt;
+class ForStmt;
+class ReadFuncStmt;
+class WriteFuncStmt;
+
+class StmtVisitor {
+public:
+    virtual void visit(ExprStmt &stmt) = 0;
+    virtual void visit(RelExprStmt &stmt) = 0;
+    virtual void visit(AddExprStmt &stmt) = 0;
+    virtual void visit(MulExprStmt &stmt) = 0;
+    virtual void visit(UnaryExprStmt &stmt) = 0;
+    virtual void visit(PrimaryExprStmt &stmt) = 0;
+    virtual void visit(ValueStmt &stmt) = 0;
+    virtual void visit(NumberStmt &stmt) = 0;
+    virtual void visit(StrStmt &stmt) = 0;
+    virtual void visit(LValStmt &stmt) = 0;
+    virtual void visit(FuncCallStmt &stmt) = 0;
+    virtual void visit(PeriodStmt &stmt) = 0;
+    virtual void visit(ConstDeclStmt &stmt) = 0;
+    virtual void visit(VarDeclStmt &stmt) = 0;
+    virtual void visit(FuncHeadDeclStmt &stmt) = 0;
+    virtual void visit(FuncBodyDeclStmt &stmt) = 0;
+    virtual void visit(FuncDeclStmt &stmt) = 0;
+    virtual void visit(AssignStmt &stmt) = 0;
+    virtual void visit(IfStmt &stmt) = 0;
+    virtual void visit(ForStmt &stmt) = 0;
+    virtual void visit(ReadFuncStmt &stmt) = 0;
+    virtual void visit(WriteFuncStmt &stmt) = 0;
+};
