978cfc20570a77428df4c33c3a4e9c4eaceef4ae
jhc <1547676099@qq.com>
2024-02-29 20:30:42 +0800

    feat: 初始化 ir-gen

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 #include "common/log/log.hpp"
+#include "ir/ir_gen.hpp"
 #include <iostream>
 #include <ostream>
 
 int main(){
     common::g_log = new common::Log(common::LOG_LEVEL::WARN);
-    LOG_DEBUG("Debug message");
-    LOG_INFO("Info message");
-    LOG_WARN("Warn message");
-    LOG_ERROR("Error message");
-    LOG_FATAL("Fatal message");
+    // LOG_DEBUG("Debug message");
+    // LOG_DEBUG("Debug message")
+    // LOG_INFO("Info message");
+    // LOG_WARN("Warn message");
+    // LOG_ERROR("Error message");
+    // LOG_FATAL("Fatal message");
     delete common::g_log;
     return 0;
 }
