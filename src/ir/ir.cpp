184e28e59aa2995e5d49aa4d67298b7c7e1abfb6
jhc <1547676099@qq.com>
2024-03-11 12:08:39 +0800

    fix(ir): 避免 this 转智能指针, 修改 visit 接口相关 bug

--- src/ir/ir.cpp
+++ src/ir/ir.cpp
@@ -1,5 +1,6 @@
 #include "ir/ir.hpp"
 #include <memory>
+#include <iostream>
 
 namespace ir {
 // 函数类型类相关函数实现
@@ -43,53 +44,33 @@ std::map<Instruction::OpID, std::string> Instruction::op2str_ = {
     {Instruction::OpID::Write, "Write"}
 };
 
-Instruction::Instruction(std::shared_ptr<Type> ty, OpID id, unsigned num_ops, std::shared_ptr<BasicBlock> parent, bool before)
-    : Value(ty, ""), op_id_(id), num_ops_(num_ops), parent_(parent) {
+Instruction::Instruction(std::shared_ptr<Type> ty, OpID id, unsigned num_ops, bool before)
+    : Value(ty, ""), op_id_(id), num_ops_(num_ops) {
         init();        
     }
-Instruction::Instruction(const std::string name, std::shared_ptr<Type> ty, OpID id, unsigned num_ops, std::shared_ptr<BasicBlock> parent, bool before) 
-    : Value(ty, name), op_id_(id), num_ops_(num_ops), parent_(parent) {
+Instruction::Instruction(const std::string name, std::shared_ptr<Type> ty, OpID id, unsigned num_ops, bool before) 
+    : Value(ty, name), op_id_(id), num_ops_(num_ops) {
         init();
 }
 void Instruction::init() {
     operands_.resize(num_ops_, nullptr); // 此句不能删去！否则operands_为空时无法用set_operand设置操作数，而只能用push_back设置操作数！
     use_pos_.resize(num_ops_);
-    parent_.lock()->add_instruction(std::static_pointer_cast<Instruction>(get_this()));
-
 }
 
 // 全局标识符类相关函数实现
-GlobalIdentifier::GlobalIdentifier(std::shared_ptr<Type> type, const std::string name, std::shared_ptr<Module> m, bool is_const, std::shared_ptr<Literal> init_val)
-    : Value(type, name), is_const_(is_const), init_val_(init_val), parent_(m) {
-        init();
-    }
-void GlobalIdentifier::init() {
-    parent_.lock()->add_global_identifier(std::static_pointer_cast<GlobalIdentifier>(get_this()));
-}
+GlobalIdentifier::GlobalIdentifier(std::shared_ptr<Type> type, const std::string name, bool is_const, std::shared_ptr<Literal> init_val)
+    : Value(type, name), is_const_(is_const), init_val_(init_val) {}
 
 // 局部标识符类相关函数实现
-LocalIdentifier::LocalIdentifier(std::shared_ptr<Type> type, const std::string name, std::shared_ptr<Function> f, bool is_const, std::shared_ptr<Literal> init_val)
-    : Value(type, name), is_const_(is_const), init_val_(init_val), parent_(f) {
-        init();
-    }
-void LocalIdentifier::init() {
-    parent_.lock()->add_local_identifier(std::static_pointer_cast<LocalIdentifier>(get_this()));
-}
+LocalIdentifier::LocalIdentifier(std::shared_ptr<Type> type, const std::string name,bool is_const, std::shared_ptr<Literal> init_val)
+    : Value(type, name), is_const_(is_const), init_val_(init_val) {}
 
 // 函数类相关函数实现
-Function::Function(std::shared_ptr<FunctionType> type, const std::string name, std::shared_ptr<Module> m) 
-    : Value(type, name), parent_(m), func_type_(type) {
-        init();
+Function::Function(std::shared_ptr<FunctionType> type, const std::string name, std::vector<std::string> arg_name) 
+    : Value(type, name), func_type_(type) {
+        size_t num_args = func_type_.lock()->args_.size();
+        for (size_t i = 0; i < num_args; i++) {
+            args_.push_back(std::make_shared<Argument>(func_type_.lock()->args_[i], arg_name[i], i));
+        }
     }
-
-void Function::init() {
-    size_t num_args = func_type_.lock()->args_.size();
-    for (size_t i = 0; i < num_args; i++) {
-        args_.push_back(std::make_shared<Argument>(func_type_.lock()->args_[i], "", std::static_pointer_cast<Function>(get_this()), i));
-    }
-    parent_.lock()->add_function(std::static_pointer_cast<Function>(get_this()));
-}
-
-
-
 } // namespace ir
--- src/ir/ir_gen.hpp
+++ src/ir/ir_gen.hpp
@@ -27,17 +27,20 @@ public:
     bool is_global();
 
     // 加入一个符号
-    bool push(const std::string &name, Value *value);
+    void push(const std::string &name, std::shared_ptr<Value> value);
 
     // 查找一个符号
-    Value *find(const std::string &name);
+    std::shared_ptr<Value> find(const std::string &name);
 
     // 当前作用域的函数
     std::shared_ptr<Function> current_f_ = nullptr;
+
+    // 打印作用域的符号
+    void print();
 private:
     // 栈式存储分配
     // 是一个 vector, 每个元素相当于作用域 map, 作用域 map 中存储了当前作用域的符号
-    std::vector<std::map<std::string, Value *>> symbols_;
+    std::vector<std::map<std::string, std::shared_ptr<Value>>> symbols_;
 };
 
 
@@ -78,6 +81,52 @@ public:
 
     Scope scope_; // 作用域
     Module module_; // 中间代码
+
+    void show_result() {
+        std::cout << "全局标识符如下:" << std::endl;
+        for (const auto &global : module_.global_identifiers_) {
+            if (global->is_const_) {
+                std::cout << global->type_->print() << " " << global->name_ << " = " << global->init_val_.lock()->print() << std::endl;
+            } else {
+                std::string s = global->type_->print();
+                int ps = s.find(" ");
+                if (ps == std::string::npos) {
+                    std::cout << global->type_->print() << " " << global->name_ << std::endl;
+                } else {
+                    std::cout << s.substr(0, ps) << " " << global->name_ << s.substr(ps + 1) << std::endl;
+                }
+            }
+        }
+        // 外层遍历函数
+        std::cout << "-----------------------------------------" << "\n";
+        for (int i = 0; i < module_.functions_.size(); i++) {
+            std::shared_ptr<Function> func = module_.functions_[i];
+            std::cout << "第 " << i + 1  << " 个函数信息如下：" << std::endl;
+            std::cout << "函数头：" << func->print() << std::endl;
+            std::cout << "局部标识符如下:" << std::endl;
+            for (const auto &local : func->local_identifiers_) {
+                if (local->is_const_) {
+                    std::cout << local->type_->print() << " " << local->name_ << " = " << local->init_val_.lock()->print() << std::endl;
+                } else {
+                    std::cout << local->type_->print() << " " << local->name_ << std::endl;
+                }
+            }
+            std::cout << "基本块如下:" << std::endl;
+            for (int j = 0; j < func->basic_blocks_.size(); j++) {
+                std::shared_ptr<BasicBlock> bb = func->basic_blocks_[j];
+                std::cout << "第 " << j + 1 << " 个基本块信息如下：" << std::endl;
+                std::cout << "基本块名：" << bb->name_ << std::endl;
+                std::cout << "指令列表如下：" << std::endl;
+                for (int k = 0; k < bb->instructions_.size(); k++) {
+                    std::shared_ptr<Instruction> inst = bb->instructions_[k];
+                    std::cout << inst->print() << "          ";
+                }
+                std::cout << "\n";
+            }
+            
+            std::cout << "-----------------------------------------" << "\n";
+        }
+    }
     
     IRGenerator() {
         // 进入全局作用域
