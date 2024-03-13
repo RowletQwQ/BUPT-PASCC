3cd790aaca4ba0ec00c83e32c91f515b495f410a
YT N <2474095154@qq.com>
2024-03-13 11:33:14 +0800

    新增常数表达式优化相关声明

---
+++ src/opt/const_expr.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "opt.hpp"
+
+namespace opt {
+class ConstExprOpt: public Optimize {
+public:
+    ConstExprOpt() = default;
+    virtual ~ConstExprOpt() = default;
+    void optimize(ir::Module &program) override;
+
+}; // class ConstExprOpt
+
+} // namespace opt
