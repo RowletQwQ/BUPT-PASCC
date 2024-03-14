731ab185d4e4a50423af2e3cf6e0fd7b0b586dfc
TY Nong <60682022+RowletQwQ@users.noreply.github.com>
2024-03-14 14:42:09 +0800

    Expr Generator optimize (#53)
    * System automatically submitted
    
    * 更改表达式解析，避免过深的表达式树
    
    ---------
    
    Co-authored-by: 农腾跃 <pfo6kwamv@educoder.net>

--- test/ir_test/run.sh
+++ test/ir_test/run.sh
@@ -5,7 +5,7 @@ for file in $test_dir/*; do
     if [ -f $file ]; then
         if [ ${file##*.} = pas ]; then
             echo "Testing $file"
-            ../bin/IR_TEST -i $file -d 4 > $(basename $file).ir || exit_status=$?
+            ../bin/IR_TEST -i $file > $(basename $file).ir || exit_status=$?
             echo "Testing $file done"
         fi
     fi
