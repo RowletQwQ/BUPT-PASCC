a3b98646971a13e51ea7ecc8e572066d9ce8aeb8
YT N <2474095154@qq.com>
2024-03-15 17:54:00 +0800

    更新测试脚本

--- test/ir_test/test.py
+++ test/ir_test/test.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(description='Test script.')
 parser.add_argument('--test_dir', type=str, default='../hidden_set',
@@ -13,14 +14,16 @@ exit_status = 0
 for file in os.listdir(test_dir):
     if os.path.isfile(os.path.join(test_dir, file)) and file.endswith('.pas'):
         print(f'Testing {file}')
+        output = ''
         try:
             output = subprocess.check_output(['../bin/IR_TEST', '-i', os.path.join(test_dir, file)], stderr=subprocess.STDOUT)
             print(output.decode('utf-8'))
+            sys.stdout.flush()
             print(f'Testing {file} done')
         except subprocess.CalledProcessError as e:
             print(f'Testing {file} failed')
-            print(output.decode('utf-8'))
             print(e.output.decode('utf-8'))
+            sys.stdout.flush()
             exit_status = e.returncode
 
 exit(exit_status)
