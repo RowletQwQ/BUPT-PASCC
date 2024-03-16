d8dca0393ee03cc1292d59b7abe131f4cd56a043
jhc <1547676099@qq.com>
2024-03-17 01:00:00 +0800

    fix: 修复分号、运算符相关 bug

--- test/ir_test/run.sh
+++ test/ir_test/run.sh
@@ -1,14 +1,19 @@
-#!/bin/bash
-test_dir=../hidden_set
-exit_status=0
+# 测试目录
+test_dir=../open_set
+
+# 输出目录
+output_dir=../test/ir_test/output
+
+# 进入到 bin 目录
+cd ../../bin
+
+# 执行测试
 for file in $test_dir/*; do
     if [ -f $file ]; then
         if [ ${file##*.} = pas ]; then
             echo "Testing $file"
-            ../bin/IR_TEST -i $file -d 4 > $(basename $file).ir || exit_status=$?
+            ./pascc -i $file -d 4 > ../test/ir_test/output/$(basename $file).ir
             echo "Testing $file done"
         fi
     fi
 done
-exit $exit_status
-
