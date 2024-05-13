8483c645e30501835b97300c2b221dceb40ecfce
Mono312 <houpengkun0801@163.com>
2024-05-13 23:42:41 +0800

    错误集补充

--- error_set/1_const_decl.pas
+++ error_set/1_const_decl.pas
@@ -1,22 +1,9 @@
-poragam 1;   // err 1
-
+program main;
 const
-  x = 10;
-  a == 2;  // err 2
-
+  x == 10;
 var
   a, b: integer;
-  c: ; // err 3
-
-function set_a(val: integer): ; // err 4
 begin
-  a := ;    // err 5
-  set_a == val; // err 6
-end;
-
-begin
-  1 := ; // err 7
-  a := ; // err 8
-  integer := 3  // err 9
-  a := 4;
-end
+  b := x;
+  a := 1;
+end.
